a84717872d203ce0a8c2777035550d3e9ed69c2a
Vaibhav Hatiskar <vhatiskar@gmail.com>
2023-12-03 20:16:30 +0530

    Spec updated to fail in before hook

--- cypress-reporter-configuration/cypress/e2e/spec.cy.js
+++ cypress-reporter-configuration/cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
 describe('template spec', () => {
+  before(function(){
+    cy.log("Should skip other test")
+    cy.fixture("./test")
+  })
   it('passes', () => {
     cy.visit('http://localhost:3000/')
   })
+  it('skip', () => {
+  cy.log("This test will get Skipped")
+})
+
 })
