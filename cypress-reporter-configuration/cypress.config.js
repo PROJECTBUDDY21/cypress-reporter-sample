e6455578a288554d74e5843a9ede92a33db8d02b
Vaibhav Hatiskar <vhatiskar@gmail.com>
2023-12-03 20:16:00 +0530

    Cypress config updated for new reporter

--- cypress-reporter-configuration/cypress.config.js
+++ cypress-reporter-configuration/cypress.config.js
@@ -3,18 +3,22 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     "reporter": "cypress-multi-reporters",
-  "reporterOptions": {
-    "reporterEnabled": "mocha-junit-reporter,mochawesome",
-    "mochaJunitReporterOptions": {
-      "mochaFile": "JUnitResult/test.xml",
-      "testCaseSwitchClassnameAndName": false
-    },
-    "mochawesomeReporterOptions": {
-      "reportDir": "MochaResult",
-      "quiet": true,
-      "html":false,
-      "json": true,
-    },
+    "reporterOptions": {
+      "reporterEnabled": "mocha-junit-reporter,mochawesome,cypress-junit-reporter",
+      "mochaJunitReporterOptions": {
+        "mochaFile": "JUnitResult/mocha-junit-results-[hash].xml",
+        "testCaseSwitchClassnameAndName": false
+      },
+      "mochawesomeReporterOptions": {
+        "reportDir": "MochaResult",
+        "quiet": true,
+        "html": false,
+        "json": true,
+      },
+      "cypressJunitReporterReporterOptions": {
+        "mochaFile": 'CypressJUnitResults/results-[hash].xml',
+        "toConsole": true,
+      }
+    }
   }
-}
 });
